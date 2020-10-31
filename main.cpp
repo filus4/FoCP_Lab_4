be4ac795b9c89c7cfeb404de80b132eb3885e7de
marcel <filej1025@gmail.com>
2020-10-31 09:43:41 +0100

    Null pointer

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int variable = 8;
+
+    int* ptr; // empty ---> null pointer
+    // cout << *ptr << endl; -- Don't uncomment, will blow up!
+
+    cout << ptr << endl;
+
+    return 0;
+}
